897a153183a9bbee1681b54265b8444af9b2f7bc
NTX <NTX@zoznam.sk>
2012-03-10 00:49:19 +0100

    Add missing cmake template file

---
+++ revision.h.in.cmake
@@ -0,0 +1,8 @@
+#ifndef __Revision
+#define __Revision
+
+    #define __LongCommitHash  "@_GIT_LONG_COMMIT_HASH@"
+    #define __ShortCommitHash "@_GIT_SHORT_COMMIT_HASH@"
+    #define __CommitDate "@_GIT_COMMIT_DATE@"
+
+#endif // __Revision
